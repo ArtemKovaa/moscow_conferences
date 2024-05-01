79b120e0d7224fd97f679d780f7bcca62a0a6146
a.k.kovalenko <a.k.kovalenko@tinkoff.ru>
2024-05-01 15:15:58 +0300

    add models

--- backend/moscow_conferences_api/models.py
+++ backend/moscow_conferences_api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    id = models.AutoField(auto_created=True, primary_key=True, serialize=False)
+    name = models.CharField(max_length=50, blank=False)
+    address = models.CharField(max_length=150, unique=True, blank=False)
+
+
+class Conference(models.Model):
+    id = models.AutoField(auto_created=True, primary_key=True, serialize=False)
+    name = models.CharField(max_length=50, unique=True, blank=False)
+    description = models.CharField(max_length=500)
+    start_date = models.DateTimeField(blank=False)
+    location = models.ForeignKey(
+        Location,
+        on_delete=models.PROTECT,
+        blank=False
+    )

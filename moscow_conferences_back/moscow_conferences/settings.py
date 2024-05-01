a9d1512df5eb1e7aaae9d20dd7531bfe6bc8c175
a.k.kovalenko <a.k.kovalenko@tinkoff.ru>
2024-05-01 12:51:39 +0300

    create and configure api django app

--- moscow_conferences_back/moscow_conferences/settings.py
+++ moscow_conferences_back/moscow_conferences/settings.py
@@ -37,9 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'moscow_conferences_api.apps.MoscowConferencesApiConfig',
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',

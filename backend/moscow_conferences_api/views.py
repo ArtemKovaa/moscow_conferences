4f7fef0315b8252bf5fb769b2cfd25f7c1941cb8
a.k.kovalenko <a.k.kovalenko@tinkoff.ru>
2024-05-22 22:54:35 +0300

    break cohesion between modules

--- backend/moscow_conferences_api/views.py
+++ backend/moscow_conferences_api/views.py
@@ -4,13 +4,14 @@ from rest_framework.response import Response
 from rest_framework import status, viewsets
 
 from moscow_conferences_api.serializers import ConferenceSerializerDetailed, ConferenceSerializerBrief
-
 from db_module.conference_dao import ConferenceDao
-from db_module.utils.conference_exception import ConferenceException
+from moscow_conferences_api.conference_exception import ConferenceException
 from rest_framework.views import exception_handler
+from moscow_conferences_api.models import Conference
 
 
 class ConferencesViewSet(viewsets.ModelViewSet):
+
     conference_dao = ConferenceDao()
 
     @csrf_exempt
@@ -29,14 +30,17 @@ class ConferencesViewSet(viewsets.ModelViewSet):
 
     @csrf_exempt
     def retrieve(self, _, pk=None):
-        conference = self.conference_dao.get_by_id(pk)
+        result = self.conference_dao.get_by_id(pk)
+        validate_existence(result, pk)
+        conference = Conference(result[0][0], result[0][1], result[0][2], result[0][3], result[0][4])
         conference_serializer = ConferenceSerializerDetailed(conference)
         return Response(conference_serializer.data, status=status.HTTP_200_OK)
 
     @csrf_exempt
     def partial_update(self, request, pk=None):
         conference_data = JSONParser().parse(request)
-        self.conference_dao.get_by_id(pk)
+        result = self.conference_dao.get_by_id(pk)
+        validate_existence(result, pk)
         conference_serializer = ConferenceSerializerDetailed(data=conference_data)
         if conference_serializer.is_valid():
             self.conference_dao.update_record(pk, conference_serializer.data)
@@ -45,8 +49,15 @@ class ConferencesViewSet(viewsets.ModelViewSet):
 
     @csrf_exempt
     def destroy(self, _, pk=None):
+        result = self.conference_dao.get_by_id(pk)
+        validate_existence(result, pk)
         self.conference_dao.delete_by_id(pk)
         return Response(status=status.HTTP_200_OK)
+
+
+def validate_existence(result, conference_id):
+    if len(result) == 0:
+        raise ConferenceException(detail=f'Conference with id={conference_id} was not found', status_code=404)
 
 
 def handle_exception(exc, context):
